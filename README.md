ceea10641b3477372f898ac1479861291e96ecee
sinemakinci1 <sinemakinci@microsoft.com>
2024-02-14 12:40:16 -0800

    Adding initial files to the repository

---
+++ README.md
@@ -0,0 +1,26 @@
+# my-cmake-project
+
+This is a simple hello world project created using CMake.
+
+## Requirements
+
+- CMake (version 3.10 or higher)
+
+## Build Instructions
+
+1. Clone the repository: `git clone https://github.com/your-username/my-cmake-project.git`
+2. Navigate to the project directory: `cd my-cmake-project`
+3. Create a build directory: `mkdir build`
+4. Navigate to the build directory: `cd build`
+5. Generate the build files using CMake: `cmake ..`
+6. Build the project: `cmake --build .`
+
+## Usage
+
+After building the project, run the executable `my-cmake-project` to see the "Hello, World!" message printed to the console.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+<!-- This file intentionally left blank -->
---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+// main.cpp
+#include <iostream>
+#include <cmath> // Include the cmath library for NaN
+using namespace std;
+
+class Calculator {
+public:
+    static double add(double num1, double num2) {
+        double added_result = num1 + num2;
+        return added_result;
+    }
+
+    static double subtract(double num1, double num2) {
+        double subtracted_result = num1 - num2;
+        return subtracted_result;
+    }
+
+    static double multiply(double num1, double num2) {
+        double multiplied_result = num1 * num2;
+        return multiplied_result;
+    }
+
+    static double divide(double num1, double num2) {
+        double divided_result = 0;
+        if (num2 != 0) {
+            divided_result = num1 / num2;
+            return divided_result;
+        }
+        else {
+            cerr << "Error! Division by zero is not allowed." << endl;
+            divided_result = std::numeric_limits<double>::quiet_NaN();
+            return divided_result;
+        }
+    }
+};
+
+int main() {
+    Calculator calculator;
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (operation) {
+    case '+':
+        cout << "Result: " << calculator.add(num1, num2) << endl;
+        break;
+    case '-':
+        cout << "Result: " << calculator.subtract(num1, num2) << endl;
+        break;
+    case '*':
+        cout << "Result: " << calculator.multiply(num1, num2) << endl;
+        break;
+    case '/':
+        cout << "Result: " << calculator.divide(num1, num2) << endl;
+        break;
+    default:
+        cout << "Invalid operation!" << endl;
+    }
+
+    return 0;
+}
