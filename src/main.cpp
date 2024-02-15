d2f1cbff968b52058b430e8de836777ee8429a3d
Sinem Akinci <99284450+sinemakinci1@users.noreply.github.com>
2024-02-15 13:45:29 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -37,34 +37,34 @@ public:
 
 
 
-int main() {
-    Calculator calculator;
-    double num1, num2;
-    char operation;
+// int main() {
+//     Calculator calculator;
+//     double num1, num2;
+//     char operation;
 
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter operation (+, -, *, /): ";
-    cin >> operation;
-    cout << "Enter second number: ";
-    cin >> num2;
+//     cout << "Enter first number: ";
+//     cin >> num1;
+//     cout << "Enter operation (+, -, *, /): ";
+//     cin >> operation;
+//     cout << "Enter second number: ";
+//     cin >> num2;
 
-    switch (operation) {
-    case '+':
-        cout << "Result: " << calculator.add(num1, num2) << endl;
-        break;
-    case '-':
-        cout << "Result: " << calculator.subtract(num1, num2) << endl;
-        break;
-    case '*':
-        cout << "Result: " << calculator.multiply(num1, num2) << endl;
-        break;
-    case '/':
-        cout << "Result: " << calculator.divide(num1, num2) << endl;
-        break;
-    default:
-        cout << "Invalid operation!" << endl;
-    }
+//     switch (operation) {
+//     case '+':
+//         cout << "Result: " << calculator.add(num1, num2) << endl;
+//         break;
+//     case '-':
+//         cout << "Result: " << calculator.subtract(num1, num2) << endl;
+//         break;
+//     case '*':
+//         cout << "Result: " << calculator.multiply(num1, num2) << endl;
+//         break;
+//     case '/':
+//         cout << "Result: " << calculator.divide(num1, num2) << endl;
+//         break;
+//     default:
+//         cout << "Invalid operation!" << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
