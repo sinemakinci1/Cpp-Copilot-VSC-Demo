d37bf1d1b1b2e37b8d27839346b2b59afb3573fe
Sinem Akinci <sinemakinci@microsoft.com>
2024-08-20 11:19:36 -0700

    updating some logic

--- src/Calculator.h
+++ src/Calculator.h
@@ -1,28 +1,15 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
 #include <iostream>
 #include <cmath> // Include the cmath library for NaN
 
 class Calculator {
 public:
-    static double add(double num1, double num2) {
-        return num1 + num2;
-    }
-
-    static double subtract(double num1, double num2) {
-        return num1 - num2;
-    }
-
-    static double multiply(double num1, double num2) {
-        return num1 * num2;
-    }
-
-    static double divide(double num1, double num2) {
-        if (num2 != 0) {
-            return num1 / num2;
-        } else {
-            std::cerr << "Error! Division by zero is not allowed.\n";
-            return std::numeric_limits<double>::quiet_NaN();
-        }
-    }
-
-
+    double add(double num1, double num2);
+    double subtract(double num1, double num2);
+    double multiply(double num1, double num2);
+    double divide(double num1, double num2);
 };
+
+#endif // CALCULATOR_H
--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,4 +1,26 @@
 #include "Calculator.h"
+#include <limits> // Include limits for NaN
+
+double Calculator::add(double num1, double num2) {
+    return num1 + num2;
+}
+
+double Calculator::subtract(double num1, double num2) {
+    return num1 - num2;
+}
+
+double Calculator::multiply(double num1, double num2) {
+    return num1 * num2;
+}
+
+double Calculator::divide(double num1, double num2) {
+    if (num2 != 0) {
+        return num1 / num2;
+    } else {
+        std::cerr << "Error! Division by zero is not allowed.\n";
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+}
 
 int main() {
     Calculator calculator;
@@ -29,4 +51,5 @@ int main() {
         std::cout << "Invalid operation!" << "\n";
     }
 
+    return 0;
 }
