79b01d4b90e009783a82df5e8eadf773a67f613c
AlexandraKemperMS <alkemper@microsoft.com>
2024-08-29 08:29:53 -0700

    insert errors

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -13,7 +13,7 @@ double Calculator::multiply(double num1, double num2) {
     return num1 * num2;
 }
 
-double Calculator::divide(double num1, double num2) {
+double Calculator::divide(int num, int num2) {
     if (num2 != 0) {
         return num1 / num2;
     } else {
