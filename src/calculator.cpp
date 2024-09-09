87e008a2fa2e861f145a48b9438ce5410c7ea10d
Sinem Akinci <sinemakinci@microsoft.com>
2024-09-09 11:06:05 -0700

    updating variable names

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,21 +1,21 @@
 #include "Calculator.h"
 #include <limits> // Include limits for NaN
 
-double Calculator::add(double num1, double num2) {
-    return num1 + num2;
+double Calculator::add(double wx_num1, double wx_num2) {
+    return wx_num1 + wx_num2;
 }
 
-double Calculator::subtract(double num1, double num2) {
-    return num1 - num2;
+double Calculator::subtract(double wx_num1, double wx_num2) {
+    return wx_num1 - wx_num2;
 }
 
-double Calculator::multiply(double num1, double num2) {
-    return num1 * num2;
+double Calculator::multiply(double num1, double wx_num2) {
+    return num1 * wx_num2;
 }
 
-double Calculator::divide(double num1, double num2) {
-    if (num2 != 0) {
-        return num1 / num2;
+double Calculator::divide(double num1, double wx_num2) {
+    if (wx_num2 != 0) {
+        return num1 / wx_num2;
     } else {
         std::cerr << "Error! Division by zero is not allowed.\n";
         return std::numeric_limits<double>::quiet_NaN();
